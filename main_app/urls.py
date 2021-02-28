918c455c56cbddd507f942166c17b3ecfc47764a
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-02-27 18:33:35 -0600

    adds form styling

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('trips/', views.trips_index, name='trips_index'),
-    path('trips/<int:trips_id>/', views.trips_detail, name='trips_detail'),
+    path('trips/<int:trip_id>/', views.trips_detail, name='trips_detail'),
     path('trips/create/', views.TripCreate.as_view(), name='trips_create'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -2,10 +2,6 @@ from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Trip
 
-class TripCreate(CreateView):
-	model = Trip
-	fields = ['destination', 'depart', 'arrive', 'hotel', 'budget', 'description']
-
 def home(request):
     return render(request, 'home.html')
 
@@ -16,5 +12,9 @@ def trips_index(request):
 def trips_detail(request, trip_id):
     trip = Trip.objects.get(id=trip_id)
     return render(request, 'trips/detail.html', { 'trip': trip })
+
+class TripCreate(CreateView):
+	model = Trip
+	fields = ['destination', 'depart', 'arrive', 'hotel', 'budget', 'description']
 
 
