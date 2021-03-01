737ad5f5f7c5ec24622209cc5431ca7e7c600838
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-03-01 16:26:46 -0600

    adds flight search api and page

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('trips/', views.trips_index, name='trips_index'),
     path('trips/<int:trip_id>/', views.trips_detail, name='trips_detail'),
     path('trips/create/', views.TripCreate.as_view(), name='trips_create'),
+    path('flight_search/', views.flight_search, name='flight_search'),
 ]
