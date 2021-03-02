ffbce6eb080f81a85e94481e988932cea9a4d39a
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-03-01 18:51:03 -0600

    adds addtl styling to app

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('trips/', views.trips_index, name='trips_index'),
-    path('trips/<int:trip_id>/', views.trips_detail, name='trips_detail'),
+    path('trips/<int:trip_id>/', views.trips_detail, name='detail'),
     path('trips/create/', views.TripCreate.as_view(), name='trips_create'),
     path('flight_search/', views.flight_search, name='flight_search'),
 ]
