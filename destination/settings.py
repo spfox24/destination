85645a5ea60e5a9b7f9c96f4717adb7e48cde4a0
spfox24 <68411909+spfox24@users.noreply.github.com>
2021-03-15 09:46:39 -0500

    prepares for deployment

--- destination/settings.py
+++ destination/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-#l@)#swy!=q&#&xwx$5ji((!vn^gmn!6zwid&t6!0w5=&q7c7'
+SECRET_KEY = os.getenv('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
